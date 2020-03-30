8377e48a76aa97b59f90e11ff10eead93303bb3b
cihan_ukan <cihan.ukan@gmail.com>
2020-03-30 22:27:55 +0300

    Added MD5 hash algorithm to secure login and register

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const ejs = require("ejs");
 
 const app = express();
@@ -24,7 +25,9 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]});
+
+//Encryption 
+//userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]});
 
 //Model
 const User = mongoose.model("User", userSchema);
@@ -44,7 +47,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         name: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(function(err){
@@ -62,7 +65,7 @@ app.get("/logout",function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({name: username}, function(err, foundUser){
         if(err){
